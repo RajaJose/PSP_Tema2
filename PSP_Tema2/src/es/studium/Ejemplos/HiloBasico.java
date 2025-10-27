5fecdcfa911f22f75dffa0ebce41ba4971afddc9
RajaJose <postigolopezandres@gmail.com>
2025-10-27 09:25:15 +0100

    ete

---
+++ PSP_Tema2/src/es/studium/Ejemplos/HiloBasico.java
@@ -0,0 +1,27 @@
+package es.studium.Ejemplos;
+import java.util.Random;
+public class HiloBasico extends Thread
+{
+	String nombre;
+	Random aleatorio = new Random();
+	public HiloBasico(String n)
+	{
+		nombre = n;
+	}
+	public void run()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.println("Hilo "+nombre+" en fase "+i);
+			try
+			{
+				sleep(aleatorio.nextInt(10)*1000);
+			}
+			catch(InterruptedException ie)
+			{
+				System.out.println("Despertando prematuramente...");
+			}
+		}
+		System.out.println("Hilo "+nombre+" ha acabado");
+	}
+}
